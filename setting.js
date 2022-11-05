9d674538db86fd53992c7918b45d48dc4c3cfa5f
AgilzElite <113176792+AgilzElite@users.noreply.github.com>
2022-11-05 13:41:08 +0800

    Update setting.js

--- setting.js
+++ setting.js
@@ -10,18 +10,18 @@ global.Api = {
 global.ApiKey = 'coderevolution' //JIKA INGIN MENDAPATKAN KOMISI DAFTAR DI HTTPS://AMPIBISMM.MY.ID UNTUK MENDAPATKAN APIKEY!
 
 global.smm = {
-  nomor : '6283102650464',
-  atasnama : 'Ahmad Nur Fadhil'
+  nomor : '6285857537269',
+  atasnama : 'Gilzz(Ranzz)'
 }
 
 // Other
-global.owner = ['6283102650464',"6282135724604"]
-global.premium = ['6283102650464']
-global.profilebio = ['6283102650464'] // ini ganti nomor lu ntar bisa nampilin pp lu
+global.owner = ['6285857537269',"6281236167286"]
+global.premium = ['6285857537269']
+global.profilebio = ['6285857537269'] // ini ganti nomor lu ntar bisa nampilin pp lu
 global.sponsor = 'no sponsors in:('
-global.packname = 'Crizzy-Bot'
-global.footer = '© 2022 Fadhil Graphy'
-global.author = 'Owner : Fadhil Graphy\nIg1 : @fdhlgrphy\nIg2 : @nr.fdhl__'
+global.packname = 'Ranzz Botzな'
+global.footer = '© 2022 Ranzz X 4K Sanzz'
+global.author = 'Owner : Ranzz(Gilzz)\nIg1 : @agilz1120\nIg2 : @Gadaaaa'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.act = {
 		wait: 'please wait a moment, processing data...',
